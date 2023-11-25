4462dab0cd5a32f1024444f520c27c5a7268d3d9
Munseonghyun <hysung2588@gmail.com>
2023-11-26 02:31:32 +0900

    HelloResponseDto 수정

--- src/main/java/com/sh/book/web/dto/HelloResponseDto.java
+++ src/main/java/com/sh/book/web/dto/HelloResponseDto.java
@@ -1,13 +1,4 @@
 package com.sh.book.web.dto;
 
 public record HelloResponseDto(String name, int amount) {
-    @Override
-    public String name() {
-        return name;
-    }
-
-    @Override
-    public int amount() {
-        return amount;
-    }
 }
